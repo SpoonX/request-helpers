e4b2e71061df0a77d7742723b7666156a52651f3
RWOverdijk <r.w.overdijk@gmail.com>
2015-07-28 17:14:41 +0200

    Updated tests

--- test/index.test.js
+++ test/index.test.js
@@ -21,10 +21,12 @@ describe('Request helpers', function () {
 
     req.setParams(testParams);
 
-    helpers.pickParams(['foo', 'bar', 'cow'], req, function (error, collected) {
-      assert.notStrictEqual(error, null, 'There is no error.');
-      done();
-    });
+    var params = helpers.secureParameters(['foo', 'bar', 'cow'], req);
+
+    assert.strictEqual(params.isValid(), false);
+    assert.strictEqual(params.getMissingParameter(), 'cow');
+
+    done();
   });
 
   it('should tell me exactly what parameter is missing', function (done) {
@@ -32,20 +34,47 @@ describe('Request helpers', function () {
 
     req.setParams(testParams);
 
-    helpers.pickParams(['foo', 'bar', 'cow'], req, function (error, collected) {
-      assert.strictEqual(error, 'cow', 'Error does not equal cow.');
-      done();
-    });
+    var params = helpers.secureParameters(['foo', 'bar', 'cow', 'clam'], req);
+
+    assert.strictEqual(params.isValid(), false);
+    assert.equal(params.getMissingParameter(), 'cow', 'Error does not equal cow.');
+    done();
+  });
+
+  it('should tell me exactly what parameters are missing', function (done) {
+    var testParams = {foo: 1, bar: 2, bat: 3, baz: 4, bacon: 5};
+
+    req.setParams(testParams);
+
+    var params = helpers.secureParameters(['foo', 'bar', 'cow', 'clam'], req, true);
+
+    assert.strictEqual(params.isValid(), false);
+    assert.deepEqual(params.getMissing(), ['cow', 'clam'], 'Missing does not equal cow.');
+    assert.equal(params.getMissing()[0], 'cow', 'Missing does not equal cow.');
+    assert.equal(params.getMissing()[1], 'clam', 'Missing does not equal clam.');
+    done();
+  });
+
+  it('should return a POJO', function (done) {
+    var testParams = {foo: 1, bar: 2, bat: 3, baz: 4, bacon: 5};
+
+    req.setParams(testParams);
+
+    var params = helpers.secureParameters(['foo', 'bar', 'bacon'], req);
+
+    assert.deepEqual(params.asObject(), {foo: 1, bar: 2, bacon: 5});
+
+    done();
   });
 
   it('should default to apple', function (done) {
     req.setParams({});
 
-    helpers.pickParams([{param: 'defaultMe', default: 'apple'}], req, function (error, collected) {
-      assert.strictEqual(null, error, 'There is an error: ' + error);
-      assert.strictEqual(collected.defaultMe, 'apple', 'Default not equal');
+    var params = helpers.secureParameters([{param: 'defaultMe', default: 'apple'}], req);
 
-      done();
-    });
+    assert.deepEqual([], params.getMissing(), 'There are missing parameters');
+    assert.strictEqual(params.get('defaultMe'), 'apple', 'Default not equal');
+
+    done();
   });
 });
