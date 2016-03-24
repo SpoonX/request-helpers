86e3d653fc6db6137b79fbba5b0ecc7d1ecc451d
RWOverdijk <r.w.overdijk@gmail.com>
2016-03-24 13:32:55 +0100

    feat(project): Allow for use of objects in stead of req

--- index.js
+++ index.js
@@ -2,21 +2,25 @@ var Parameters = require('./lib/Parameters'),
     typer      = require('typer');
 
 module.exports = {
-  secureParameters : function (rules, req, forceCollect) {
+  secureParameters: function(rules, req, forceCollect) {
     var parameters = new Parameters(),
         i          = 0;
 
+    if (typeof req.param !== 'function') {
+      req.param = mockParamMethodFactory(req);
+    }
+
     for (; i < rules.length; i++) {
-      var param          = rules[i],
-          required       = true,
-          defaultValue   = null,
-          cast           = 'smart';
+      var param        = rules[i],
+          required     = true,
+          defaultValue = null,
+          cast         = 'smart';
 
       if (typeof param === 'object') {
         required = !(typeof param.required !== 'undefined' && !param.required);
 
         if (typeof param.default !== 'undefined') {
-          required = false;
+          required     = false;
           defaultValue = param.default;
         }
 
@@ -46,3 +50,17 @@ module.exports = {
     return parameters;
   }
 };
+
+function mockParamMethodFactory(object) {
+  if (typeof object['param'] !== 'undefined') {
+    object['__param'] = object['param'];
+  }
+
+  return function mockParamMethod(key) {
+    if (key === 'param') {
+      key = '__param';
+    }
+
+    return typeof object[key] !== 'undefined' ? object[key] : null;
+  }
+}
